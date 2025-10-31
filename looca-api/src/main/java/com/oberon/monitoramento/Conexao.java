326110ed9e461b6ca4dbe94e063640a97aa4bf8d
JhoelDiego2 <jhoel.mita@sptech.school>
2025-10-31 19:19:39 -0300

    fix: endereço ip e adiona uma janela para debug

--- looca-api/src/main/java/com/oberon/monitoramento/Conexao.java
+++ looca-api/src/main/java/com/oberon/monitoramento/Conexao.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Conexao {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/bdOberon";
+    private static final String URL = "jdbc:mysql://44.207.133.113:3306/bdOberon";
     private static final String USER = "oberon_cliente";
     private static final String PASSWORD = "ClienteOberon123"; // coloque sua senha aqui
 
